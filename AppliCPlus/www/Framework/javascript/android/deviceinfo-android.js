25e595721425d2c5ca5ec8e0b97b3c1cb58df03b
lalibe-m <maxime.laliberte@epitech.eu>
2014-04-01 14:26:45 +0200

    gros nettoyage du projet

--- AppliCPlus/www/Framework/javascript/android/deviceinfo-android.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-var pluginHandler = {
-			getDeviceLanguage: function(onSuccess, onFail){
-			return (cordova.exec(onSuccess, onFail, "fr.blueapps.bluetech.newtech.PluginHandler", "getDeviceLanguage", []));
-			}
-		};
